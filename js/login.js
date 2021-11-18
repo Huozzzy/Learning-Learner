e30e663f430028ceeee84386ce07a06dca4f0124
Huozy <42334632+Huozzzy@users.noreply.github.com>
2021-11-18 20:33:38 +0800

    v1.2.1 登陆二维码自动居中

--- js/login.js
+++ js/login.js
@@ -1,17 +1,27 @@
-let qrCode = document.querySelector('.ddloginbox');
-if (qrCode) {
-    setTimeout(function () {
-        qrCode.scrollIntoView({
-            behavior: "smooth"
-        });
-        setTimeout(function () {
-            window.scrollTo({
-                left: (document.body.scrollWidth - document.documentElement.clientWidth) / 2,
-                top: window.scrollY,
-                behavior: 'smooth'
-            });
-        }, 1000 + Math.floor(Math.random() * 1000));
-    }, 1000 + Math.floor(Math.random() * 1000))
-}
+// var qrCode = document.querySelector('.ddloginbox');
+// if (qrCode) {
+//     setTimeout(function () {
+//         qrCode.scrollIntoView({
+//             behavior: "smooth"
+//         });
+//         setTimeout(function () {
+//             window.scrollTo({
+//                 left: (document.body.scrollWidth - document.documentElement.clientWidth) / 2,
+//                 top: window.scrollY,
+//                 behavior: 'smooth'
+//             });
+//         }, 1000 + Math.floor(Math.random() * 1000));
+//     }, 1000 + Math.floor(Math.random() * 1000))
+// }
+
+setTimeout(function () {
+    window.scrollTo({
+        left: (document.body.scrollWidth - document.documentElement.clientWidth) / 2,
+        top: 900,
+        behavior: 'smooth'
+    });
+}, 10);
 
 // chrome.runtime.sendMessage({"method": "checkLogin"});
+
+
