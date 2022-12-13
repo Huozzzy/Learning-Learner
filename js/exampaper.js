8f41f8b5774c33ed73de31b0eb8713c0d0e12698
Huozy <42334632+Huozzzy@users.noreply.github.com>
2022-12-13 14:21:59 +0800

    v3.0.4-pre 解决滑动验证块问题

--- js/exampaper.js
+++ js/exampaper.js
@@ -1,6 +1,19 @@
 chrome.runtime.sendMessage({ type: "checkRunning" }, {}, function (response) {
     if (response && response.hasOwnProperty("runtime")) {
         if (response.runtime) {
+            // 滑块验证
+            const swiperValid = document.getElementById("swiper_valid")
+            if (swiperValid) {
+                let obs = new MutationObserver(mutationRecords => {
+                    slideVerify()
+                  });
+                  obs.observe(swiperValid, {
+                    childList: true,
+                    subtree: true,
+                    characterDataOldValue: true
+                  });
+            }
+
             function getNeedAnswer() {
                 var isNextPage = true;
                 document.querySelectorAll('.item .right > button').forEach(function (e, b, c) {
@@ -66,6 +79,45 @@ chrome.runtime.sendMessage({ type: "checkRunning" }, {}, function (response) {
             //         }
             //     }
             // }
+
+            function slideVerify() {
+                const nc_mask = document.getElementById("nc_mask")
+                if (nc_mask !== null && getComputedStyle(nc_mask).display !== 'none') {
+                    var btn_slide = document.getElementById("nc_1_n1z")
+                    var mousedown = document.createEvent("MouseEvents");
+                    if (btn_slide !== null) {
+                        var btn_slide_rect = btn_slide.getBoundingClientRect();
+                        var x = btn_slide_rect.left + btn_slide_rect.width * (parseInt(Math.random() * 5) / 10 + 0.2);
+                        var y = btn_slide_rect.top + btn_slide_rect.height / 2;
+                    }
+                    var nc_scale = document.getElementById("nc_1_n1t");
+                    if (nc_scale !== null) { var w = nc_scale.getBoundingClientRect().width; }
+
+                    //点击滑块
+                    mousedown.initMouseEvent("mousedown", true, true, window, 0, x, y, x, y, false, false, false, false, 0, null);
+                    if (btn_slide != null) { btn_slide.dispatchEvent(mousedown); }
+
+                    var aa = 0;
+                    var bb = 0;
+                    //滑动滑块
+                    var intervaltimer = setInterval(function () {
+                        var mousemove = document.createEvent("MouseEvents");
+                        var _x = x + aa;
+                        var _y = y + bb;
+                        mousemove.initMouseEvent("mousemove", true, true, window, 0, _x, _y, _x, _y, false, false, false, false, 0, null);
+                        if (btn_slide != null) { btn_slide.dispatchEvent(mousemove); }
+                        if (_x - x >= w) {
+                            // 滑到最右边
+                            clearInterval(intervaltimer);
+                            var mouseup = document.createEvent("MouseEvents");
+                            mouseup.initMouseEvent("mouseup", true, true, window, 0, _x, _y, _x, _y, false, false, false, false, 0, null);
+                            if (btn_slide != null) { btn_slide.dispatchEvent(mouseup); }
+                        } else {
+                            aa += parseInt(Math.random() * (209 - 199) + 199) / 33;
+                        }
+                    }, 10);
+                }
+            }
 
             chrome.storage.local.get(['studyConfig'], function (result) {
             //     let config = result.studySubjectConfig;
