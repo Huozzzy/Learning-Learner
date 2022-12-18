435ff6f84d486b669b46b834969a259427036196
Huozy <42334632+Huozzzy@users.noreply.github.com>
2022-12-18 14:13:58 +0800

    v3.0.4-pre1.3 修复验证加载时无法取值的问题

--- js/exampractice.js
+++ js/exampractice.js
@@ -214,66 +214,42 @@ chrome.runtime.sendMessage({ type: 'checkRunning' }, {}, function (response) {
           var btn_slide = document.getElementById('nc_1_n1z')
           if (btn_slide !== null) {
             var btn_slide_rect = btn_slide.getBoundingClientRect()
-            var x =
-              btn_slide_rect.left +
-              btn_slide_rect.width * (parseInt(Math.random() * 5) / 10 + 0.2)
+            var x = btn_slide_rect.left + btn_slide_rect.width * (parseInt(Math.random() * 5) / 10 + 0.2)
             x = x.toFixed(2)
-            console.log(x)
+            // console.log('x=' + x)
             var y = btn_slide_rect.top + btn_slide_rect.height / 2
-          }
-          var nc_scale = document.getElementById('nc_1_n1t')
-          if (nc_scale !== null) {
-            var w = nc_scale.getBoundingClientRect().width
-          }
-          const eventdown = new MouseEvent('mousedown', {
-            bubbles: true,
-            cancelable: true,
-            view: window,
-            detail: 0,
-            screenX: x,
-            screenY: y,
-            clientX: x,
-            clientY: y,
-            ctrlKey: false,
-            altKey: false,
-            shiftKey: false,
-            metaKey: false,
-            button: 0,
-            relatedTarget: null,
-          });
-          if (btn_slide != null) {
-            btn_slide.dispatchEvent(eventdown)
-          }
 
-          var offsetX = 0
-          //滑动滑块
-          var intervaltimer = setInterval(function () {
-            // var mousemove = document.createEvent('MouseEvents')
-            var _x = x + offsetX
-            var _y = y
-            const eventmove = new MouseEvent('mousemove', {
+            var nc_scale = document.getElementById('nc_1_n1t')
+            if (nc_scale !== null) {
+              var w = nc_scale.getBoundingClientRect().width
+            }
+            const eventdown = new MouseEvent('mousedown', {
               bubbles: true,
               cancelable: true,
               view: window,
               detail: 0,
-              screenX: _x,
-              screenY: _y,
-              clientX: _x,
-              clientY: _y,
+              screenX: x,
+              screenY: y,
+              clientX: x,
+              clientY: y,
               ctrlKey: false,
               altKey: false,
               shiftKey: false,
               metaKey: false,
               button: 0,
               relatedTarget: null,
-            })
+            });
             if (btn_slide != null) {
-              btn_slide.dispatchEvent(eventmove)
+              btn_slide.dispatchEvent(eventdown)
             }
-            if (_x - x >= w) {
-              // 滑到最右边
-              clearInterval(intervaltimer);
-              const eventup = new MouseEvent('mouseup', {
+
+            var offsetX = 0
+            //滑动滑块
+            var intervaltimer = setInterval(function () {
+              // var mousemove = document.createEvent('MouseEvents')
+              var _x = x + offsetX
+              var _y = y
+              const eventmove = new MouseEvent('mousemove', {
                 bubbles: true,
                 cancelable: true,
                 view: window,
@@ -288,15 +264,38 @@ chrome.runtime.sendMessage({ type: 'checkRunning' }, {}, function (response) {
                 metaKey: false,
                 button: 0,
                 relatedTarget: null,
-              });
+              })
               if (btn_slide != null) {
-                btn_slide.dispatchEvent(eventup)
+                btn_slide.dispatchEvent(eventmove)
               }
-            } else {
-              offsetX += (parseInt((Math.random() * (209 - 199) + 199) / 33)).toFixed(2)
-              console.log('offsetX='+offsetX)
-            }
-          }, 20)
+              if (_x - x >= w) {
+                // 滑到最右边
+                clearInterval(intervaltimer);
+                const eventup = new MouseEvent('mouseup', {
+                  bubbles: true,
+                  cancelable: true,
+                  view: window,
+                  detail: 0,
+                  screenX: _x,
+                  screenY: _y,
+                  clientX: _x,
+                  clientY: _y,
+                  ctrlKey: false,
+                  altKey: false,
+                  shiftKey: false,
+                  metaKey: false,
+                  button: 0,
+                  relatedTarget: null,
+                });
+                if (btn_slide != null) {
+                  btn_slide.dispatchEvent(eventup)
+                }
+              } else {
+                offsetX += (parseInt((Math.random() * (209 - 199) + 199) / 33)).toFixed(2)
+                // console.log('offsetX=' + offsetX)
+              }
+            }, 30)
+          }
         }
       }
 
